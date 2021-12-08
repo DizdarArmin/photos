2d8df09cf4a90e2642590ed84af6eb8f012763d3
Armin Dizdar <armin.dizdar@gmail.com>
2021-12-08 11:53:45 +0100

    initial colaboration push test

--- App.tsx
+++ App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import 'text-encoding';
-import 'react-native-get-random-values'
+import 'react-native-get-random-values';
 import 'react-native-polyfill-globals/auto';
-import "react-native-wasm";
+import 'react-native-wasm';
 import AppNavigation from './navigation/AppNavigation';
-import {
-  RecoilRoot,
-} from 'recoil';
-import { LogBox } from 'react-native';
-import { RootSiblingParent } from 'react-native-root-siblings';
-
+import {RecoilRoot} from 'recoil';
+import {LogBox} from 'react-native';
+import {RootSiblingParent} from 'react-native-root-siblings';
 
 LogBox.ignoreLogs(['Setting a timer']);
 
 const App = () => {
-  return (
-    <RecoilRoot>
+	return (
+		<RecoilRoot>
 			<RootSiblingParent>
-      	<AppNavigation />
+				<AppNavigation />
+				//init push
 			</RootSiblingParent>
-    </RecoilRoot>
-  );
+		</RecoilRoot>
+	);
 };
 
 export default App;
